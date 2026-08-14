cc6a1614b5bafbd06ae64d99a8c84d16e1cd6ca0
valentina-ignacia <va.moral@duocuc.cl>
2026-08-14 12:22:20 -0400

    2nd commit

--- src/main/java/valentinaignacia/Main.java
+++ src/main/java/valentinaignacia/Main.java
@@ -2,13 +2,18 @@ package valentinaignacia;
 
 public class Main {
     static void main(String[] args) {
-        /* Creación objeto */
-        var libro1 = new Libro("Harry Potter", "JKR", false);
 
-        /* Correr métodos */
+        /* Caso 1 Ejercicio 1 */
+        var libro1 = new Libro("Harry Potter", "JKR", false);
         libro1.leer();
         libro1.getDatos();
-
+        if (libro1.cambiarDisponibilidad()) {
+            String estado = "Disponible";
+            System.out.println("Estado del libro: " + estado);
+        } else {
+            String estado = "No Disponible";
+            System.out.println("Estado del libro: " + estado);
+        }
         if (libro1.cambiarDisponibilidad()) {
             String estado = "Disponible";
             System.out.println("Estado del libro: " + estado);
@@ -17,13 +22,6 @@ public class Main {
             System.out.println("Estado del libro: " + estado);
         }
 
-        if (libro1.cambiarDisponibilidad()) {
-            String estado = "Disponible";
-            System.out.println("Estado del libro: " + estado);
-        } else {
-            String estado = "No Disponible";
-            System.out.println("Estado del libro: " + estado);
-        }
-
+        /* Caso 1 Ejercicio 2 */
     }
 }
--- src/main/java/valentinaignacia/Prestamo.java
+++ src/main/java/valentinaignacia/Prestamo.java
@@ -3,15 +3,38 @@ package valentinaignacia;
 public class Prestamo {
     /* Atributos */
     String nombreEstudiante;
-    String libro;
+    String nombreLibro;
     boolean estDevolucion;
-    int diaPrestamo;
-    int mesPrestamo;
+    int fechaPrestamo;
+    int fechaDevolucion;
 
     /*Constructor*/
+    public Prestamo (String nombreEstudiante, String nombreLibro,
+                     int fechaPrestamo, int fechaDevolucion) {
 
+        this.nombreEstudiante = nombreEstudiante;
+        this.nombreLibro = nombreLibro;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.estDevolucion = false;
+    }
 
     /*Métodos*/
+    public boolean estadoDevolucion() { /*Comprueba si se ha devuelto el libro o no*/
+        return estDevolucion;
+    }
+
+    public void devolverLibro() { /*Se usa cuando se devuelve el libro*/
+        estDevolucion = true;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Estudiante: " + nombreEstudiante);
+        System.out.println("Libro: " + nombreLibro);
+        System.out.println("Fecha préstamo: " + fechaPrestamo);
+        System.out.println("Fecha devolución: " + fechaDevolucion);
+        System.out.println("¿Devuelto?: " + estDevolucion);
+    }
 
 
 }
